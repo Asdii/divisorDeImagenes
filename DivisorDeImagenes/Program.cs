e397717db18edc3b40d19320c85fbc937248b287
Ferret <42761019+Asdii@users.noreply.github.com>
2019-11-04 01:02:43 -0300

    Add files via upload

---
+++ DivisorDeImagenes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace DivisorDeImagenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombreArchivo = args[0];
+            string[] coleccion = nombreArchivo.Split('.');
+            string[] coleccionAbsoluta = coleccion[0].Split('\\');
+            Image img = Image.FromFile(nombreArchivo);
+            int widthThird = (int)((double)img.Width / Convert.ToInt32(coleccionAbsoluta[coleccionAbsoluta.Length - 1]));
+            int heightThird = (int)((double)img.Height);
+            Bitmap[] bmps = new Bitmap[16];
+
+            for (int i = 0; i < Convert.ToInt32(coleccionAbsoluta[coleccionAbsoluta.Length - 1]); i++) {
+                    bmps[i] = new Bitmap(widthThird, heightThird);
+                    Graphics g = Graphics.FromImage(bmps[i]);
+                    g.DrawImage(img, new Rectangle(0, 0, widthThird, heightThird), new Rectangle( i * widthThird, 0, widthThird, heightThird), GraphicsUnit.Pixel);
+                    g.Dispose();
+
+                    bmps[i].Save("Frame " + Convert.ToString(i + 1) + ".png");
+            }
+        }
+    }
+}
